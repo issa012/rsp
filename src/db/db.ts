ccc3a37b0c65c7a3d0fa2849f403f20793bc9d32
Yeldar <57894795+issa012@users.noreply.github.com>
2023-08-22 15:35:32 +0600

    db config

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,15 @@
 import { DataSource } from 'typeorm';
-
+import dotenv from 'dotenv';
+dotenv.config();
 export const db = new DataSource({
   type: 'postgres',
-  host: 'db',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['src/entity/*.{js,ts}'],
   logging: true,
   synchronize: true,
+  ssl: true,
 });
