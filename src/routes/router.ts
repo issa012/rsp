fc47e85ee551842914d6c0db514f32ab9c51f881
Yeldar <57894795+issa012@users.noreply.github.com>
2023-08-23 14:51:16 +0600

    logging headers

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -9,7 +9,8 @@ router.get('/', (req: Request, res: Response) => {
 
 router.post('/guest-login', async function (req: Request, res: Response) {
   if (req.get('Guest-Login')) {
-    let user = JSON.parse(req.get('Guest-Login'));
+    let user = JSON.parse(req.header('Guest-Login'));
+    console.log(req.headers);
     const results = await findUser(user.id);
     return res.json(results);
   }
